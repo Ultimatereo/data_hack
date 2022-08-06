209253a38de5bf763cb3b20b67b8f29d5bf0676f
Marlesss <334874@niuitmo.ru>
2022-08-06 14:06:38 +0300

    Table class loading added

--- main.py
+++ main.py
@@ -21,10 +21,22 @@
 # spark.table()
 
 
-from data import *
+import importlib
 import os
 import json
+from fields import *
 
+
+def get_table_class(path, class_name):
+    m = importlib.import_module(path)
+    c = getattr(m, class_name)
+    return c
+
+
+table_class = get_table_class("CellClass", "Cell")
+a = table_class()
+print(generate(a))
+"""
 a = Cell()
 
 b = Cell2()
@@ -41,3 +53,4 @@ print("_______")
 print(generate_paired(a, b, ['float3', 'integer4']))
 print(generate(a))
 print(generate(b))
+"""
