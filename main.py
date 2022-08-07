776b41549148efd5ecdb92bf06c120973a420fe0
Marlesss <334874@niuitmo.ru>
2022-08-07 04:04:28 +0300

    App config handling
    jobs "solo_generate" & "show_data" added

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ spark = SparkSession.builder.appName('Data_hack').getOrCreate()
 
 
 def get_table_class(path, class_name):
-    m = importlib.import_module(path)
+    if path.endswith(".py"):
+        path = path[:-3]
+    m = importlib.import_module("dataclass." + path)
     c = getattr(m, class_name)
     return c
 
@@ -22,13 +24,16 @@ def load_table(table_script: str, table_class_name: str):
     return table_class()
 
 
-def load_config(table, table_class_name: str, config_path="config.json"):
+def load_table_config(table, table_class_name: str, config_path: str):
+    if config_path is None:
+        return
     try:
-        with open(config_path, "r") as config:
+        with open(f"config/table/{config_path}", "r") as config:
             apply_changes(table, table_class_name, json.load(config))
     except FileNotFoundError as e:
         print(f"Config '{config_path}' not found")
         print(e)
+        print("Continue with default settings...")
     except Exception as e:
         print(f"Error during applying config '{config_path}' to table '{table_class_name}'")
         print(e)
@@ -45,33 +50,48 @@ def apply_changes(table, table_class_name, changes: dict):
         setattr(table, field_name, getattr(table, field_name).apply_changes(table_changes.get(field_name)))
 
 
-def export_dataframe(df, path, export_type, export_mode):
+def export_dataframe(df, dir_name, export_type, export_mode):
     if export_type not in DATA_TYPES:
         raise Exception(f"Unknown export_type '{export_type}'. It must be one of {DATA_TYPES}")
     if export_mode not in EXPORT_MODES:
         raise Exception(f"Unknown export_mode '{export_mode}'. It must be one of {EXPORT_MODES}")
-    df.write.format(export_type).mode(export_mode).save(path)
+    df.write.format(export_type).mode(export_mode).save(dir_name)
 
 
-def solo_generate(table_script_path, table_class_name, export_path='output', export_type='parquet',
-                  export_mode='overwrite'):
-    table = load_table(table_script_path, table_class_name)
-    load_config(table, table_class_name)
+def solo_generate(table_script_name, table_class_name, table_config, dir_name, export_type, export_mode):
+    if dir_name is None:
+        dir_name = "output"
+    if export_type is None:
+        export_type = "parquet"
+    if export_mode is None:
+        export_mode = "overwrite"
+    table = load_table(table_script_name, table_class_name)
+    load_table_config(table, table_class_name, table_config)
     data = generator(table)
     rdd = spark.sparkContext.parallelize(data)
     df = rdd.toDF(list(fields_names(table)))
-    export_dataframe(df, export_path, export_type, export_mode)
+    export_dataframe(df, dir_name, export_type, export_mode)
 
 
-def intersect_generate(table1_script_path, table1_class_name,
-                       table2_script_path, table2_class_name,
+def intersect_generate(table1_script_name, table1_class_name,
+                       table2_script_name, table2_class_name,
                        intersect_keys: dict,
-                       export_path1='output1', export_path2='output2',
-                       export_type='parquet', export_mode='overwrite'):
-    table1 = load_table(table1_script_path, table1_class_name)
-    table2 = load_table(table2_script_path, table2_class_name)
-    load_config(table1, table1_class_name)
-    load_config(table2, table2_class_name)
+                       table_config,
+                       export_path1, export_path2,
+                       export_type, export_mode):
+    if export_path1 is None:
+        export_path1 = "output1"
+    if export_path2 is None:
+        export_path2 = "output2"
+    if export_type is None:
+        export_type = "parquet"
+    if export_mode is None:
+        export_mode = "overwrite"
+
+    table1 = load_table(table1_script_name, table1_class_name)
+    table2 = load_table(table2_script_name, table2_class_name)
+    load_table_config(table1, table1_class_name, table_config)
+    load_table_config(table2, table2_class_name, table_config)
     data1, data2 = paired_generator(table1, table2, intersect_keys)
     rdd = spark.sparkContext.parallelize(data1)
     df1 = rdd.toDF(list(fields_names(table1)))
@@ -81,12 +101,14 @@ def intersect_generate(table1_script_path, table1_class_name,
     export_dataframe(df2, export_path2, export_type, export_mode)
 
 
-def show_data(path, data_type="parquet", count=20):
+def show_data(dir_name, data_type, count):
+    if count is None:
+        count = 20
     if data_type not in DATA_TYPES:
         raise Exception(f"Unknown data_type '{data_type}'. It must be one of {DATA_TYPES}")
-    df = spark.read.format(data_type).load(path)
+    df = spark.read.format(data_type).load(dir_name)
     df.show(count)
-    print(df.count())
+    print("Total rows:", df.count())
     df.printSchema()
 
 
@@ -114,15 +136,55 @@ def play_around():
     print(dt3.date())
 
 
+def make_task(task: dict):
+    def check_required_args(names: List[str], args: dict):
+        for name in names:
+            if name not in args:
+                raise Exception(f"You must give '{name}' in 'args'")
+
+    def make_show_data(args: dict):
+        check_required_args(["dir_name", "data_type"], args)
+        dir_name = args.get("dir_name")
+        data_type = args.get("data_type")
+        count = args.get("count", None)
+        show_data(dir_name, data_type, count)
+
+    def make_solo_generate(args: dict):
+        check_required_args(["table", "export"], args)
+        table = args.get("table")
+        check_required_args(["script_name", "class_name"], table)
+        export = args.get("export")
+        check_required_args(["dir_name"], export)
+        script_name = table.get("script_name")
+        class_name = table.get("class_name")
+        dir_name = export.get("dir_name")
+        export_type = export.get("export_type", None)
+        export_mode = export.get("export_mode", None)
+        table_config = args.get("config", None)
+        solo_generate(script_name, class_name, table_config, dir_name, export_type, export_mode)
+
+    MAKERS = {"show_data": make_show_data,
+              "solo_generate": make_solo_generate}
+
+    if "job" not in task:
+        raise Exception("There is no job in task")
+    job = task.get("job")
+    if job not in MAKERS:
+        raise Exception(f"Unknown job '{job}'. You can only call {MAKERS.keys()}")
+    maker = MAKERS.get(job)
+    args = task.get("args", {})
+    print(f"{job} started")
+    maker(args)
+    print(f"{job} finished")
+
+
 if __name__ == '__main__':
     try:
-        # solo_generate("CellClass", "Cell", "CellGenCSV", "csv")
-        intersect_generate("CellClass", "Cell",
-                           "Cell2Class", "Cell2",
-                           {"integer1": "iid", "float2": "acceleration", "abcword": "word"},
-                           export_type="parquet")
-        # show_data("CellGen", "parquet")
-        show_data("output1", "parquet")
-        show_data("output2", "parquet")
+        with open("config/app/default.json", "r") as app_config_file:
+            app_config = json.load(app_config_file)
+        if "tasks" not in app_config:
+            raise Exception("There are no tasks in the app config")
+        for task in app_config.get("tasks"):
+            make_task(task)
     except Exception as e:
         print(e)
