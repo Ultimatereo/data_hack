3a7fd737eaa1d1e937fbd9796944c1fc60b4e070
Артём Швецов <shvetsovart55@gmail.com>
2022-08-07 10:54:06 +0300

    anime config fix

--- dataclass/AnimeSeriesClass.py
+++ dataclass/AnimeSeriesClass.py
@@ -8,4 +8,7 @@ class AnimeSeries:
     series_name: str = StringRange(10, 30, string.ascii_letters)
     series_duration: int = IntegerRange(20, 25)
     episodes_number_in_season: int = IntegerRange(10, 12)
-    seasons_number: int = IntegerRange(100, 200)
+    seasons_number: int = IntegerRange(6, 12)
+    main_tyan_number: int = IntegerRange(4, 7)
+    producer_id: int = IntegerRange(1, 50)
+    release_date: date = Date()
